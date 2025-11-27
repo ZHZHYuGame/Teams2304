52da4676a01bcd800c655d7408e3d848c5be5b74
Zxy <925901252@qq.com>
2025-11-27 21:40:44 +0800

    11.27AB包热更代码

---
+++ 11.27作业/张新宇11.27/VersionData.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// °æ±¾ºÅ
+/// </summary>
+public class VersionData 
+{
+    public int big;
+    public int middle;
+    public int small;
+    public string vStr;
+    public VersionData(string version)
+    {
+        vStr = version;
+        string[] verArr = version.Split('.');
+        big = int.Parse(verArr[0]);
+        middle = int.Parse(verArr[1]);
+        small = int.Parse(verArr[2]);
+    }
+    public override string ToString()
+    {
+        return vStr;
+    }
+}
