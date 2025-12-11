611d88cf0517ac6390dca4d2287b6bf9a6399834
Han <2761862280@qq.com>
2025-12-11 21:37:22 +0800

    Lua虚拟机

---
+++ Lua/LuaMain.lua
@@ -0,0 +1,39 @@
+require("Lipus/BaseClass")
+require("Lipus/head")
+require("Message/Net_MessageControll")
+require("Net/NetManager")
+require("Lipus/LuaUtil")
+NetManager:Init()
+print("baseclass = ", _G.BaseClass)
+_G.UImgr = require("Lua_Manager/UIManager")
+_G.UImgr:Init()
+require("GameEnum/UILayer")
+require("GameEnum/UITypeEnum")
+require("UI/UIConfigMgr")
+require("Net/NetID")
+
+
+local bagbtn
+function LuaStart()
+    -- print("going lua..............")
+
+    -- _G.UImgr:ShowUI(UITypeEnum.shop)
+    --  _G.UImgr:ShowUI(UITypeEnum.bag)
+   
+    -- bagbtn = GameObject.Find("bagbtn"):GetComponent("Button")
+    -- bagbtn.onClick:AddListener(function()
+       
+    --      _G.UImgr:ShowUI(UITypeEnum.bag)
+      
+    -- end)
+    -- local msg = MyGame.C_To_S_Chat_Msg()
+    -- netManager.GetInstance():SendMessage(NetID.C_To_S_WorldChat_Msg, Protobuf.ToByteArray(msg))
+end
+
+function LuaUpdata()
+
+end
+
+--local c_Chat_Msg = MyGame.C_To_S_Chat_Msg()
+
+--print("chat msg = ", c_Chat_Msg)
---
+++ Scripts/LuaMain.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using XLua;
+
+public class LuaMain : Singleton<LuaMain>
+{
+    LuaEnv lua=new LuaEnv();
+    public Action start, updata;
+    // Start is called before the first frame update
+   public void Start()
+    {
+        lua.AddLoader(Cust);
+        lua.DoString("require 'LuaMain'");
+        start = lua.Global.Get<Action>("LuaStart");
+        updata = lua.Global.Get<Action>("LuaUpdata");
+        start?.Invoke();
+    }
+
+    private byte[] Cust(ref string filepath)
+    {
+        return File.ReadAllBytes(Application.dataPath + "/LuaMain/" + filepath + ".lua");
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        updata?.Invoke();
+    }
+}
