031d8dd37406847136d5689c8a2d1da1663e8746
DONGJIANLEI <2765696255@qq.com>
2025-12-12 21:26:18 +0800

    已修改NetID内容

--- Script/Lua/Net/NetID.lua
+++ Script/Lua/Net/NetID.lua
@@ -1,10 +1,6 @@
 NetID = {
-     C_To_S_GetShopInfos_msg = 1015,
-     S_To_C_GetShopInfos_msg = 1016,
-     C_To_S_BuyShop_msg = 1017,
-     S_To_C_BuyShop_msg = 1018,
-     C_To_S_GetPlayerInfo_msg = 1019,
-     S_To_C_GetPlayerInfo_msg = 1020,
-     S_To_C_RedPoint_Update_msg = 1021,
-     C_To_S_RedPoint_Update_msg = 1022
+     C_To_S_GetPlayerData_Msg = 1001,
+     S_To_C_GetPlayerData_Msg = 1002,
+     C_To_S_UseBagItem_Msg = 1003,
+     S_To_C_UseBagItem_Msg = 1004
 }
