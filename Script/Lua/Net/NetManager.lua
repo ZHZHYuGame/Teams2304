446e8488c2c198bc8eb7db7195e7543757976d70
DONGJIANLEI <2765696255@qq.com>
2025-12-12 21:24:45 +0800

    Net/NetManager.lua添加代码

--- Script/Lua/Net/NetManager.lua
+++ Script/Lua/Net/NetManager.lua
@@ -1,10 +1,13 @@
 Lua_NetManager = BaseClass("NetManager")
 
 function Handle_Net_Msg(netData)
-
-    NetMessageControll:Dispatch(netData.netID, netData.byteData)
-
-    
+    local byteData = {}
+    if netData.netID == NetID.S_To_C_GetPlayerData_Msg then
+        byteData = MyGame.S_To_C_GetPlayerData_Msg.Parser:ParseFrom(netData.byteData)
+    elseif netData.netID == NetID.S_To_C_UseBagItem_Msg then
+        byteData = MyGame.S_To_C_UseBagItem_Msg.Parser:ParseFrom(netData.byteData)
+    end
+    NetMessageControll:Dispatch(netData.netID, byteData)
 end
 
 function Lua_NetManager:Init()
