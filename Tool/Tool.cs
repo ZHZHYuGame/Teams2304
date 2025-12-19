393d0d66994a201686bae4bf38239bd04df81ee2
馨香楠 <2163172284@qq.com>
2025-12-19 15:04:19 +0800

    12.19

--- Tool/Tool.cs
+++ Tool/Tool.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using Newtonsoft.Json;
 
 public class Tool : MonoBehaviour,IPointerDownHandler
 {
     public Action<PointerEventData> Action_OnPointerDown;
+    public static Dictionary<string,string>logdic=new Dictionary<string,string>();
     public static Tool AddTool(GameObject obj)
     {
         var my_obj = obj.GetComponent<Tool>();
@@ -24,5 +27,11 @@ public class Tool : MonoBehaviour,IPointerDownHandler
     public void OnPointerDown(PointerEventData eventData)
     {
         Action_OnPointerDown?.Invoke(eventData);
+    }
+    public static void SaveLogData(string name,string pass)
+    {
+        logdic.Add(name, pass);
+        string json=JsonConvert.SerializeObject(logdic);
+        File.WriteAllText(Application.dataPath+ "/Resources/Log.json", json);
     }
 }
