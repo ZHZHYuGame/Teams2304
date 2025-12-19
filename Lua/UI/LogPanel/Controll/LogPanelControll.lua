ff8c44abee5c14eeab38f1dc336e2b745ccb4dbd
馨香楠 <2163172284@qq.com>
2025-12-19 15:02:55 +0800

    12.19

--- Lua/UI/LogPanel/Controll/LogPanelControll.lua
+++ Lua/UI/LogPanel/Controll/LogPanelControll.lua
@@ -10,6 +10,8 @@ end
 
 function LogPanelControll:S_To_C_Log_Handel(type)
 	self.type = type[1].Type
+	self.name = type[1].UserName
+	self.pass = type[1].PassWord
 	if self.type == MyGame.RegisterOrLogEndType.UserNameNull then
 		print("账号不能为空")
 	elseif self.type == MyGame.RegisterOrLogEndType.PassWordNull then
@@ -20,6 +22,12 @@ function LogPanelControll:S_To_C_Log_Handel(type)
 		print("密码错误")
 	elseif self.type == MyGame.RegisterOrLogEndType.Success then
 		print("登录成功")
+		_G.UImgr:CloseUI(UITypeEnum.log, false)
+		CS.Tool.SaveLogData(self.name, self.pass)
+		SceneManager.LoadScene("SampleScene")
+		_G.UImgr:ShowUI(UITypeEnum.main)
+		_G.UImgr:ShowUI(UITypeEnum.shop)
+		_G.UImgr:ShowUI(UITypeEnum.bag)
 	end
 end
 
--- Lua/UI/RegisterPanel/Controll/RegisterPanelControll.lua
+++ Lua/UI/RegisterPanel/Controll/RegisterPanelControll.lua
@@ -8,7 +8,7 @@ function RegisterPanelControll:AddListener()
 	NetMessageControll:AddListener(NetID.S_To_C_Register, Bind(self, self.S_To_C_Register_Handel))
 end
 
-function LogPanelControll:S_To_C_Register_Handel(type)
+function RegisterPanelControll:S_To_C_Register_Handel(type)
 	self.type = type[1].Type
 	if self.type == MyGame.RegisterOrLogEndType.UserNameNull then
 		print("账号不能为空")
@@ -18,6 +18,7 @@ function LogPanelControll:S_To_C_Register_Handel(type)
 		print("账号已注册")
 	elseif self.type == MyGame.RegisterOrLogEndType.Success then
 		print("注册成功")
+		_G.UImgr:CloseUI(UITypeEnum.register, false)
 	end
 end
 
