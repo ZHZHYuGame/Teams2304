99470e3fedc53de5e55329cd3695f6a8000a0c54
aaaa <212656667@qq.com>
2025-12-19 19:59:16 +0800

    CreatPlayer脚本更新

--- 客户端脚本更新/CreatPlayer.cs
+++ 客户端脚本更新/CreatPlayer.cs
@@ -6,15 +6,18 @@ using UnityEngine;
 using Google.Protobuf;
 using Google;
 using MyGame;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CreatPlayer : MonoBehaviour
 {
     public static CreatPlayer instance;
-    public Dictionary<uint,Player>  PlayerDic = new Dictionary<uint, Player>();
-    public Player player;
+    public Dictionary<uint,GameObject>  PlayerDic = new Dictionary<uint, GameObject>();
+    public GameObject player;
     public static uint PlayerID ; 
-    public CinemachineFreeLook playerCamera;
-
+    public CinemachineFreeLook playerCamera3;
+    
     private void Awake()
     {
         if (instance == null)
@@ -25,6 +28,7 @@ public class CreatPlayer : MonoBehaviour
         
         MessageControll.GetInstance().AddListener(NetID.S_To_C_PlayerOperation,RushPlayer);
         MessageControll.GetInstance().AddListener(NetID.S_To_C_Disconnect,S_To_C_DisconnectHandle);
+        MessageControll.GetInstance().AddListener(NetID.S_To_C_ExitGame,S_To_C_ExitGameHandle);
         BulletMgr.GetInstance().Init();
         HPManger.GetInstance().Init();
         PlayerAnimatorMgr.GetInstance().Init();
@@ -32,6 +36,21 @@ public class CreatPlayer : MonoBehaviour
         Instantiate(ABManager.GetInstance().LoadAsset_GameObject("map"));
 
     }
+
+    private void S_To_C_ExitGameHandle(object obj)
+    {
+        object[] objs = obj as object[];
+        Byte[] bytes = objs[0] as Byte[];
+        
+        S_To_C_ExitGame scMsg = S_To_C_ExitGame.Parser.ParseFrom(bytes);
+
+        if (PlayerDic.ContainsKey(scMsg.PlayerId))
+        {
+            Destroy(PlayerDic[scMsg.PlayerId]);
+            PlayerDic.Remove(scMsg.PlayerId);
+        }
+    }
+
     /// <summary>
     /// 断开链接
     /// </summary>
@@ -50,22 +69,36 @@ public class CreatPlayer : MonoBehaviour
         }
     }
 
+    [SerializeField] private Button tui;
     void Start()
     {
+        
+        tui.onClick.AddListener(() =>
+        {
+            //告诉其他客户端，自己退出游戏了
+            PlayerNetMgr.GetInstance().PlayerExitGame();
+            SceneManager.LoadScene("Main");
+            //打开主面板
+            LuaMgr.GetInstance().lua.Global.Get<Action>("InitMain")();
+        });
         C_To_S_PlayerOperation msg=new C_To_S_PlayerOperation();
         msg.PlayerId = PlayerID;
-        player = Instantiate(Resources.Load<Player>("Role/3"));
-        Vector2 n = UnityEngine.Random.insideUnitCircle * 10;
-        player.transform.position = new Vector3(n.x, 23, n.y)+new Vector3(70,0,40);
+        player = Instantiate(Resources.Load<GameObject>("Role/3"));
+        player.transform.position = RefreshPlayerPos();
         NetManager.GetInstance().SendMessage(NetID.C_To_S_PlayerOperation,msg.ToByteArray());
-        player.Init(PlayerID);
+        player.AddComponent<Player>().Init(PlayerID);
         
-        if (playerCamera == null)
+        if (playerCamera3 == null)
         {
-            playerCamera = Instantiate(Resources.Load<CinemachineFreeLook>("FreeLook Camera"));
+            playerCamera3 = Instantiate(Resources.Load<CinemachineFreeLook>("FreeLook Camera"));
         }
     }
 
+    public Vector3 RefreshPlayerPos()
+    {
+        Vector2 n = UnityEngine.Random.insideUnitCircle * 10;
+        return new Vector3(n.x, 23, n.y)+new Vector3(70,0,40); 
+    }
     private void RushPlayer(object obj)
     {
         object[] par = obj as object[];
@@ -78,16 +111,14 @@ public class CreatPlayer : MonoBehaviour
         }
         if (!PlayerDic.ContainsKey(msg.PlayerId))
         {
-            PlayerDic[msg.PlayerId] = Instantiate(Resources.Load<Player>("Role/3"));
+            PlayerDic[msg.PlayerId] = Instantiate(Resources.Load<GameObject>("Role/3"));
             PlayerDic[msg.PlayerId].tag = "Enemy";
         }
         else
         {
             PlayerDic[msg.PlayerId].transform.position=new Vector3(msg.X,msg.Y,msg.Z);
             PlayerDic[msg.PlayerId].transform.rotation=Quaternion.Euler(0,msg.RoundY,0);
-            
         }
-        
     }
 
     private float timer;
@@ -103,12 +134,17 @@ public class CreatPlayer : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-                ishide=true;
-                Cursor.visible = false; // 隐藏鼠标图标
-                Cursor.lockState = CursorLockMode.Locked; // 锁定鼠标到屏幕中心，且鼠标移动仅输出增量（无实际位置移动）
-                
-                playerCamera.Follow = player.transform;
-                playerCamera.LookAt = player.transform;
+            //判断是否点击到ui
+            if (EventSystem.current.IsPointerOverGameObject())
+            {
+                return;
+            }
+            ishide=true;
+            Cursor.visible = false; // 隐藏鼠标图标
+            Cursor.lockState = CursorLockMode.Locked; // 锁定鼠标到屏幕中心，且鼠标移动仅输出增量（无实际位置移动）
+            
+            playerCamera3.Follow = player.transform;
+            playerCamera3.LookAt = player.transform.Find("tou");
         }
 
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -117,8 +153,8 @@ public class CreatPlayer : MonoBehaviour
             Cursor.visible = true; // 显示鼠标图标
             Cursor.lockState = CursorLockMode.None; // 解除锁定，鼠标可自由移动
             
-            playerCamera.Follow =null;
-            playerCamera.LookAt = null;
+            playerCamera3.Follow =null;
+            playerCamera3.LookAt = null;
         }
         
     }
