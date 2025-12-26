4d3b8488023ee0c4aeab65c75aeb404c17b796fc
Zxy <925901252@qq.com>
2025-12-26 16:55:12 +0800

    1

--- Lua/UI/UIConfigMgr.lua
+++ Lua/UI/UIConfigMgr.lua
@@ -2,6 +2,8 @@ UIConfigMgr = {
     [UITypeEnum.shop] = require("UI/shop/ShopConfig"),
     [UITypeEnum.main] = require("UI/main/mainConfig"),
     [UITypeEnum.bag] = require("UI/bag/BagConfig"),
+    [UITypeEnum.selectrole]=require("UI/SelectRole/SelectRoleConfig"),
     [UITypeEnum.log] = require("UI/LogPanel/LogPanelConfig"),
-    [UITypeEnum.register] = require("UI/RegisterPanel/RegisterPanelConfig")
+    [UITypeEnum.register] = require("UI/RegisterPanel/RegisterPanelConfig"),
+    [UITypeEnum.guide] =require("UI/guidemask/guidemaskConfig")
 }
